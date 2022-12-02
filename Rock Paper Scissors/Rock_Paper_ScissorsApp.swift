9bcac66fb7a4f28e7959726427a26dc2266113e6
micahsteele <micahs66@gmail.com>
2022-12-02 15:31:43 -0500

    Initial Commit

---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Micah Steele on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
